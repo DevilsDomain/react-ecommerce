a28513fe4d3dff21e5b793aa56e32618ef8092b1
DevilsDomain <alaraerzincan@gmail.com>
2023-06-20 18:55:21 +0200

    added links to the product details

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
 
 interface ProductData {
     image: string;
     title: string;
     price: number;
+    id: Number
     rating: {
       rate: number;
     };
@@ -20,7 +22,7 @@ function ProductCard({data}: ProductCardProps) {
     <div className="h-96 w-64 border-2 border-amber-700 rounded-2xl flex items-end justify-center mb-9 text-sm">
         <div>
             <Image className='ml-10 mb-5' src={data.image} width={160} height={200} alt='desc'/>
-            <p className='pl-3 pb-1'>{data.title}</p>
+            <Link href={`/products/${data.id}`} className='pl-3 pb-1'>{data.title}</Link>
             <p className='pl-3 pb-2'>${data.price}</p>
         </div>
         <p className='pb-2 pr-3'>{data.rating.rate}</p>
