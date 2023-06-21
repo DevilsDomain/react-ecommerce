8e609eef3eb2668f832b49d1a0dbcc53557b7a2f
DevilsDomain <alaraerzincan@gmail.com>
2023-06-21 11:20:30 +0200

    added rmove button in the cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react';
 
-function page() {
+type CartItem = {
+    title: string;
+    price: number;
+  };
+  
+
+function CartPage() {
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    if (typeof window !== 'undefined') {
+      // Retrieve cart items from localStorage
+      const storedCartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+      return storedCartItems;
+    }
+    return [];
+  });
+
+  const handleRemoveItem = (index: number) => {
+    // Create a copy of the cartItems array
+    const updatedCartItems = [...cartItems];
+    // Remove the item from the copied array
+    updatedCartItems.splice(index, 1);
+    // Update the state and localStorage
+    setCartItems(updatedCartItems);
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+  };
+
   return (
-    <div>cart page</div>
-  )
+    <div>
+      <h1>Cart Page</h1>
+      {cartItems.map((item, index) => (
+        <div key={index}>
+          {/* Render the cart item details */}
+          <p>{item.title}</p>
+          <p>${item.price}</p>
+          {/* Add more fields as needed */}
+          <button onClick={() => handleRemoveItem(index)}>Remove</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default page
+export default CartPage;
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -4,7 +4,7 @@ import useSWR from 'swr';
 import Image from 'next/image';
 
 type ProductParams = {
-  params: { productId: Number };
+  params: { productId: number };
 };
 
 function Page({ params: { productId } }: ProductParams) {
@@ -21,20 +21,39 @@ function Page({ params: { productId } }: ProductParams) {
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
 
+  const handleAddToCart = () => {
+    // Retrieve cart items from localStorage
+    const cartItems: any[] = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+    // Add the product to the cart
+    cartItems.push(data);
+
+    // Update the cart items in localStorage
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+    // Log the updated cart items to the console
+    console.log(cartItems);
+  };
 
   return (
     <div className='flex flex-row'>
-        <div className='w-2/3 ml-32 mt-20'>
-            <Image src={data.image} width={350} height={350} alt='desc' />
-        </div>
-        <div className='w-1/2 -ml-96 mt-20'>
-            <h1 className='text-5xl mb-3'>{data.title}</h1>
-            <p className='text-2xl'>Stars: {data.rating.rate}</p>
-            <p className='mb-3'>Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis
-             sit aliquam sit nullam neque ultrices.</p>
-            <h2 className='text-4xl mb-10'>${data.price}</h2>
-            <button className='w-32	h-14 bg-red-200 rounded-full'>Add to Cart</button>
-        </div>
+      <div className='w-2/3 ml-32 mt-20'>
+        <Image src={data.image} width={350} height={350} alt='desc' />
+      </div>
+      <div className='w-1/2 -ml-96 mt-20'>
+        <h1 className='text-5xl mb-3'>{data.title}</h1>
+        <p className='text-2xl'>Stars: {data.rating.rate}</p>
+        <p className='mb-3'>
+          Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit
+          phasellus mollis sit aliquam sit nullam neque ultrices.
+        </p>
+        <h2 className='text-4xl mb-10'>${data.price}</h2>
+        <button
+          className='w-32 h-14 bg-red-200 rounded-full'
+          onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 }
